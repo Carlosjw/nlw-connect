d6fa0ddba0c6d06c16527865ccd26a97e1c3fc17
Carlosjw <carlos.ti.infor@gmail.com>
2025-02-18 14:41:07 -0300

    firstProjectCommit

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import './globals.css'
+
+import { Montserrat, Oxanium } from 'next/font/google'
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "devstage",
+
+};
+
+// Definindo propriedades das fontes personalizdada
+const oxanium = Oxanium({
+  weight: ['500', '600'], // peso da fonte
+  subsets: ['latin'], // quais caracteres carregar
+  variable: '--font-oxanium' // dizendo pro Tailwind qual o nome da variável que chama esta fonte
+})
+
+const montserrat = Montserrat({
+  weight: ['400', '600'],
+  subsets: ['latin'],
+  variable: '--font-montserrat'
+})
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR" className={`${oxanium.variable} ${montserrat.variable}`}>
+      <body className='bg-gray-900 text-gray-100 antialiased'>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+
+import { ArrowRight } from 'lucide-react'
+import { Button } from "@/components/button";
+import { IconButton } from '@/components/icon-button';
+
+export default function Home() {
+  return (
+    <main>
+      <Button>
+        Enviar
+        <ArrowRight />
+      </Button>
+
+      <IconButton>
+        <ArrowRight/>
+      </IconButton>
+    </main>
+  );
+}
